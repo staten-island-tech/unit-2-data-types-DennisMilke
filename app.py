b14451849d81f57c34a8481f2a3fc9bd3b92cc95
DennisMilke <dmilke923@gmail.com>
2024-09-16 10:02:28 -0400

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,8 @@ else:
 
  ## Tip Calculator
 
- x_totalBill == x_initialBill * ( 1 + 0.01 * x_tipValue)
- x_initialBill= input("What is your bill total?")
- print("How was the service?")
- input("Bad, Okay, Good, Great")
+def x_totalBill() : int(x_initialBill) + (x_initialBill * ( 1 + 0.01 * x_tipValue)) 
+
+x_initialBill = input("What is your bill total?")
+print("How was the service?")
+def x_tipValue(): input("Bad, Okay, Good, Great")
