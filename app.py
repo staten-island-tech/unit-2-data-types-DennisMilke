152dd7b4880907521469551099f6da1b717d67ca
DennisMilke <dmilke923@gmail.com>
2024-09-16 09:20:15 -0400

    app.py work unit 2

--- app.py
+++ app.py
@@ -1,1 +1,15 @@
-print("likashdfgbl")
+## Odd/Even identifiers
+
+""" x = input("Pick a number:")
+if int(x) % 2 == 0:
+    print("even")
+else:
+    print("odd")
+ """
+
+ ## Tip Calculator
+
+ x_totalBill == x_initialBill * ( 1 + 0.01 * x_tipValue)
+ x_initialBill= input("What is your bill total?")
+ print("How was the service?")
+ input( )
